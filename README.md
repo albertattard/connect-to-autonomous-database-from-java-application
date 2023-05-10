d4dad8ae8ff401c86d67522f07984ac62f36f946
Albert Attard <albert.attard@oracle.com>
2023-05-10 14:28:49 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,122 @@
+# Connect to Autonomous Database using a Wallet
+
+## Obtain the wallet
+
+1. Go to the [Autonomous Database](https://cloud.oracle.com/db/adb) and access
+   the database with which you like to connect.
+
+2. Click on the _Database Connection_ button.
+   ![Autonomous Database details - Database connection](./assets/images/Autonomous%20Database%20details%20-%20Database%20connection.png)
+
+3. Click on the _Download wallet_ button.
+   ![Autonomous Database details - Download Wallet](./assets/images/Autonomous%20Database%20details%20-%20Download%20Wallet.png)
+
+4. Provide a password to this wallet and click the _Download_ button.
+   ![Autonomous Database details - Provide Password and Download Wallet](./assets/images/Autonomous%20Database%20details%20-%20Provide%20Password%20and%20Download%20Wallet.png)
+
+   While it is important to use a strong password, it will not be used in this
+   example.
+
+5. Unzip the downloaded file.
+
+   ```shell
+   $ unzip Wallet_xxxxxx.zip
+   ```
+
+   This file can be unzipped anywhere you like, as long as it is accessible to
+   the program. I unzipped mine under the `~/Downloads/wallet` directory. Make
+   sure that the directory where the ZIP file was extracted
+   (`~/Downloads/wallet`) contains the following files.
+
+   ```shell
+   $ tree ~/Downloads/wallet
+
+   ~/Downloads/wallet
+   ├── README
+   ├── cwallet.sso
+   ├── cwallet.sso.lck
+   ├── ewallet.p12
+   ├── ewallet.p12.lck
+   ├── ewallet.pem
+   ├── keystore.jks
+   ├── ojdbc.properties
+   ├── sqlnet.ora
+   ├── tnsnames.ora
+   └── truststore.jks
+   ```
+
+   The path to the folder will be passed to the program to connect with the
+   autonomous database.
+
+## Commands
+
+- Create fat JAR file containing all dependencies in it (all in one)
+
+  The `fatJar` custom Gradle task will create one (fat) JAR file that includes
+  all the dependencies.
+
+  ```shell
+  $ ./gradlew fatJar
+  ```
+
+  (_Optional_) Verify that the JAR file was created
+
+  ```shell
+  $ tree './build/libs/'
+  ./build/libs/
+  └── app-all-in-one.jar
+  ```
+
+- Run the application
+
+  ```shell
+  $ java --class-path './build/libs/*' demo.Main
+  ```
+
+  The program will ask for some input. Provide the requested information. If the
+  provided information is correct, the program will then query the sample `SH`
+  schema and prints the results.
+
+  ```
+  12:34:56.000 [main] INFO  demo.Main -- Connecting to autonomous database: ConnectionDetails[jdbcUrl=jdbc:oracle:thin:@xxxxxx?TNS_ADMIN=/path/to/wallet, username=xxxxxx, password=xxxxxx]
+  12:34:56.138 [main] INFO  demo.Main -- Listing 20 customers from the sample 'SH' schema
+  12:34:56.252 [main] INFO  demo.Main -- Customer Id First Name Last Name City                 Credit Limit
+  12:34:56.252 [main] INFO  demo.Main -- ------------------------------------------------------------------
+  12:34:56.257 [main] INFO  demo.Main --        3228 Abigail    Ruddy     Hoofddorp            7000
+  12:34:56.257 [main] INFO  demo.Main --        4117 Abner      Everett   Clermont-l'Herault   15000
+  12:34:56.257 [main] INFO  demo.Main --        6783 Abigail    Ruddy     Schimmert            11000
+  12:34:56.258 [main] INFO  demo.Main --        7673 Abner      Everett   Schwaebisch Gmuend   11000
+  12:34:56.258 [main] INFO  demo.Main --       10338 Abigail    Ruddy     Scheveningen         1500
+  12:34:56.258 [main] INFO  demo.Main --       13894 Abigail    Ruddy     Joinville            9000
+  12:34:56.258 [main] INFO  demo.Main --       17449 Abigail    Ruddy     Nagoya               9000
+  12:34:56.258 [main] INFO  demo.Main --       21005 Abigail    Ruddy     Santos               3000
+  12:34:56.258 [main] INFO  demo.Main --       24561 Abigail    Ruddy     Yokohama             7000
+  12:34:56.258 [main] INFO  demo.Main --       25470 Abner      Everett   Stuttgart            15000
+  12:34:56.365 [main] INFO  demo.Main --       28116 Abigail    Ruddy     Haarlem              11000
+  12:34:56.365 [main] INFO  demo.Main --       31671 Abigail    Ruddy     Bolton               1500
+  12:34:56.365 [main] INFO  demo.Main --       35227 Abigail    Ruddy     Lelystad             9000
+  12:34:56.365 [main] INFO  demo.Main --       36117 Abner      Everett   Wolverhampton        15000
+  12:34:56.365 [main] INFO  demo.Main --       39672 Abner      Everett   Murnau               11000
+  12:34:56.366 [main] INFO  demo.Main --       43228 Abner      Everett   Los Angeles          7000
+  12:34:56.366 [main] INFO  demo.Main --       47006 Abner      Everett   Montara              11000
+  12:34:56.366 [main] INFO  demo.Main --       49671 Abigail    Ruddy     Ede                  1500
+  12:34:56.366 [main] INFO  demo.Main --       50561 Abner      Everett   Neuss                7000
+  ```
+
+  Otherwise, an error is printed depending on the error type. Following is an
+  example of the error printed when provided a wrong password.
+
+  ```
+  12:34:56.000 [main] INFO  demo.Main -- Connecting to autonomous database: ConnectionDetails[jdbcUrl=jdbc:oracle:thin:@xxxxxx?TNS_ADMIN=/path/to/wallet, username=xxxxxx, password=xxxxxx]
+  12:34:56.999 [main] ERROR demo.Main -- Failed to connect adn execute query
+  java.sql.SQLException: UCP-0: Unable to start the Universal Connection Pool
+      at oracle.ucp.util.UCPErrorHandler.newSQLException(UCPErrorHandler.java:399)
+      at oracle.ucp.util.UCPErrorHandler.throwSQLException(UCPErrorHandler.java:163)
+  ...
+  ```
+
+  Verify that the correct credentials are used and try again.
+
+## Pending
+
+1. Add the terraform scripts that create the Autonomous Database
---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,158 @@
+package demo;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+ * Adapted from: https://github.com/oracle-samples/oracle-db-examples/blob/main/java/jdbc/ConnectionSamples/ADBQuickStart.java
+ */
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(final String[] args) throws Exception {
+        final PoolDataSource dataSource = createDataSource();
+
+        try (Connection connection = dataSource.getConnection()) {
+            withConnection(connection);
+        } catch (final SQLException e) {
+            LOGGER.error("Failed to connect adn execute query", e);
+        }
+    }
+
+    private static void withConnection(final Connection connection) throws SQLException {
+        final String query = """
+                SELECT CUST_ID, CUST_FIRST_NAME, CUST_LAST_NAME, CUST_CITY, CUST_CREDIT_LIMIT
+                FROM SH.CUSTOMERS
+                WHERE ROWNUM < 20
+                ORDER BY CUST_ID""";
+        LOGGER.info("Listing 20 customers from the sample 'SH' schema");
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            LOGGER.info("Customer Id First Name Last Name City                 Credit Limit");
+            LOGGER.info("------------------------------------------------------------------");
+
+            while (resultSet.next()) {
+                LOGGER.info("{} {} {} {} {}",
+                        String.format("%11s", resultSet.getString(1)),
+                        String.format("%-10s", resultSet.getString(2)),
+                        String.format("%-9s", resultSet.getString(3)),
+                        String.format("%-20s", resultSet.getString(4)),
+                        String.format("%-12s", resultSet.getString(5)));
+            }
+        }
+    }
+
+    private static PoolDataSource createDataSource() throws SQLException {
+        final ConnectionDetails connectionDetails = ConnectionDetails.readFromConsole();
+        LOGGER.info("Connecting to autonomous database: {}", connectionDetails);
+
+        final PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
+        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+        dataSource.setURL(connectionDetails.jdbcUrl());
+        dataSource.setUser(connectionDetails.value);
+        dataSource.setPassword(connectionDetails.password());
+        dataSource.setConnectionPoolName("JDBC_UCP_POOL");
+        dataSource.setInitialPoolSize(1);
+        dataSource.setMinPoolSize(1);
+        dataSource.setMaxPoolSize(5);
+        return dataSource;
+    }
+
+    private record ConnectionDetails(String jdbcUrl, String value, String password) {
+        public static ConnectionDetails readFromConsole() {
+            Console console = System.console();
+            if (console == null) {
+                throw new RuntimeException("Console is not available");
+            }
+
+            final Path walletPath = readWalletPath(console);
+            final String tnsAlias = readTnsAlias(console, walletPath);
+            final String username = readUsername(console);
+            final String password = readPassword(console);
+
+            final String jdbcUrl = "jdbc:oracle:thin:@%s?TNS_ADMIN=%s".formatted(tnsAlias, walletPath.toAbsolutePath());
+            return new ConnectionDetails(jdbcUrl, username, password);
+        }
+
+        private static Path readWalletPath(final Console console) throws IllegalArgumentException {
+            String input = console.readLine("Wallet directory: ");
+
+            /* Replace ~ with the user home directory */
+            if (input.startsWith("~" + File.separator)) {
+                input = System.getProperty("user.home") + input.substring(1);
+            }
+
+            final Path walletPath = Path.of(input);
+            if (!Files.isDirectory(walletPath)) {
+                throw new IllegalArgumentException("Invalid wallet directory. The provided path is not a directory.");
+            }
+
+            return walletPath;
+        }
+
+        private static String readTnsAlias(final Console console, final Path walletPath) {
+            final Path tnsnamesPath = walletPath.resolve("tnsnames.ora");
+            if (!Files.isRegularFile(tnsnamesPath)) {
+                throw new IllegalArgumentException("Invalid wallet directory. The provided directory does not contain the 'tnsnames.ora' file.");
+            }
+
+            final List<String> tnsAliases = readTnsAliasesInFile(tnsnamesPath);
+            final String input = console.readLine("TNS Alias (one of: %s) [%s]: ", String.join(", ", tnsAliases), tnsAliases.get(0));
+            return defaultIfBlank(input, tnsAliases.get(0));
+        }
+
+        private static List<String> readTnsAliasesInFile(final Path file) {
+            try (Stream<String> lines = Files.lines(file)) {
+                final List<String> tnsAliases = lines.filter(line -> line.contains("="))
+                        .map(line -> line.split("=", 2)[0].trim())
+                        .toList();
+
+                if (tnsAliases.isEmpty()) {
+                    throw new IllegalArgumentException("The file %s does not contain TNS aliases".formatted(file.toAbsolutePath()));
+                }
+
+                return tnsAliases;
+            } catch (final IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        private static String readUsername(final Console console) {
+            final String input = console.readLine("Username [Admin]: ");
+            return defaultIfBlank(input, "Admin");
+        }
+
+        private static String readPassword(final Console console) {
+            return String.valueOf(console.readPassword("Password: "));
+        }
+
+        private static String defaultIfBlank(final String value, final String defaultValue) {
+            return (value == null || value.isBlank())
+                    ? defaultValue
+                    : value;
+        }
+
+        @Override
+        public String toString() {
+            return "ConnectionDetails[jdbcUrl=%s, username=%s, password=xxxxxx]".formatted(jdbcUrl, value);
+        }
+    }
+}
